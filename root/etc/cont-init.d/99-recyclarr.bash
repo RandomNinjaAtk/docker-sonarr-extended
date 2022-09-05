08472d92f7a20e5ee6c8bda14124210d0a05f1b1
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-09-05 19:06:43 -0400

    Update 99-recyclarr.bash

--- root/etc/cont-init.d/99-recyclarr.bash
+++ root/etc/cont-init.d/99-recyclarr.bash
@@ -5,7 +5,7 @@ echo "Setting up Recyclarr"
 # Download Recyclarr
 if [ ! -f /recyclarr/recyclarr ]; then
     mkdir -p /recyclarr
-    wget -q "https://github.com/recyclarr/recyclarr/releases/latest/download/recyclarr-linux-musl-x64.zip" -O "/recyclarr/recyclarr.zip"
+    wget -q "https://github.com/recyclarr/recyclarr/releases/latest/download/recyclarr-linux-x64.zip" -O "/recyclarr/recyclarr.zip"
     unzip -o /recyclarr/recyclarr.zip -d /recyclarr &>/dev/null
     chmod u+rx /recyclarr/recyclarr
 fi
